6a52834dfe23d277b36af48b74e2eeff5a65f0bf
Christopher Bellocci <christopher.bellocci@hotmail.com>
2023-04-20 17:42:28 +0100

    completed frontend build using chat engine pretty and edited backend and hid the api key as not to expose it

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const secret_key = process.env.APIKEY;
 const api_key = secret_key;
 
+
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
